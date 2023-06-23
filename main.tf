7f16330506473beaea7ef80913e5d42ea3107032
gousiy <129971846+gousiy@users.noreply.github.com>
2023-06-23 15:44:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region     = "eu-north-1"
+}
+
+resource "aws_instance" "my_ec2_instance" { 
+ ami = "ami-0989fb15ce71ba39e" 
+ instance_type = "t3.medium" 
+ key_name = "devopslabs" 
+  
+ tags ={ 
+ name="Terraform hands-on" 
+} 
+ user_data  = <<-EOF
+ #!/bin/bash
+     sudo apt-get update -y
+     sudo apt install docker.io -y
+     sudo systemctl enable docker
+     sudo docker run -itd -p 8084:8081 alishataj/banking
+     sudo docker start $(docker ps -aq)
+EOF
+}
